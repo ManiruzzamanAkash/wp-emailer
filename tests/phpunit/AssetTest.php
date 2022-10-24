98287e5d7c33da31982ef048e2d26a2b71f0b177
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-10-24 08:51:53 +0600

    🚨 PHPUnit test for Asset loader

---
+++ tests/phpunit/AssetTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Akash\WpEmailer\Tests;
+
+/**
+ * Assets class test.
+ *
+ * @since WP_EMAILER_SINCE
+ */
+class AssetTest extends \WP_UnitTestCase {
+
+    /**
+     * Asset class instance.
+     *
+     * @var \Akash\WpEmailer\Asset()
+     */
+    public $asset;
+
+    /**
+     * Setup test environment.
+     */
+    protected function setUp() : void {
+        $this->asset = new \Akash\WpEmailer\Asset();
+    }
+
+    public function test_get_router_base_url() {
+        $admin_page_url1 = 'http://host.com/folder/wp-admin/admin.php?page=wp-emailer';
+        $admin_page_url2 = 'http://host.com/wp-admin/admin.php?page=wp-emailer';
+
+        $this->assert_equals(
+            'wpvue/wp-admin/admin.php?page=wp-emailer#',
+            $this->asset->get_router_base_url( $admin_page_url1 )
+        );
+
+        $this->assert_equals(
+            'wp-admin/admin.php?page=wp-emailer#',
+            $this->asset->get_router_base_url( $admin_page_url2 )
+        );
+    }
+}
