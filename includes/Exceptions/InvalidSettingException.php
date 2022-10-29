1762f63844c020466fbb2e96df86830a809fff06
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-10-29 07:20:43 +0600

    New: Invalid Setting Exception

---
+++ includes/Exceptions/InvalidSettingException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Akash\WpEmailer\Exceptions;
+
+use Exception;
+
+/**
+ * Invalid Setting Exception class
+ *
+ * @since WP_EMAILER_SINCE
+ *
+ * @uses Exception Default exception class.
+ */
+class InvalidSettingException extends Exception {}
