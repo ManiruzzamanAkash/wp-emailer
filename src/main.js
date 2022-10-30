c2f58850349484798c679caff3e32e56314ddf71
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-10-31 01:01:40 +0600

    i18n and eslint setup

--- src/main.js
+++ src/main.js
@@ -7,11 +7,12 @@ import { createHooks } from "@wordpress/hooks";
 /**
  * Internal dependencies.
  */
+import './i18n';
+import i18nMixin from "./mixins/i18n";
+
 import App from "./App.vue";
 import router from "./router";
-import './i18n';
 import "./styles/main.scss";
-import i18nMixin from "./mixins/i18n";
 import './utils/menu-fix';
 import store from "./store";
 
@@ -20,9 +21,11 @@ const app = createApp({
     extends: App,
     mixins: [i18nMixin]
 });
-app.use(router);
-app.use(store);
+
 app.config.devtools = process.env.NODE_ENV === "development";
+
+app.use(router)
+    .use(store);
 
 // Finally Mount on the #wp-emailer div.
 app.mount("#wp-emailer");
@@ -30,13 +33,13 @@ app.mount("#wp-emailer");
 // Add action/filter hooks injectable
 window.wpEmailerHooks = createHooks();
 wpEmailerHooks.addFilter = (hookName, namespace, component, priority = 10) => {
-  wpEmailerHooks.hooks.addFilter(
-    hookName,
-    namespace,
-    components => {
-      components.push(component);
-      return components;
-    },
-    priority
-  );
+    wpEmailerHooks.hooks.addFilter(
+        hookName,
+        namespace,
+        components => {
+            components.push(component);
+            return components;
+        },
+        priority
+    );
 };
