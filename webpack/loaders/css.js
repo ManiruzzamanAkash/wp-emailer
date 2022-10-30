cdd8d7b1b04e2c1d14c527e127d4047188724367
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-10-31 00:57:54 +0600

    Refactor: Refactored and fixed eslint

--- webpack/loaders/css.js
+++ webpack/loaders/css.js
@@ -1,10 +1,16 @@
 const miniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  test: /\.css$/,
-  use: [
-    miniCssExtractPlugin.loader,
-    "style-loader",
-    "css-loader",
-  ]
+    test: /\.css$/,
+    use: [
+        miniCssExtractPlugin.loader,
+        "style-loader",
+        'vue-style-loader',
+        {
+            loader: 'css-loader',
+            options: {
+                esModule: false
+            },
+        },
+    ]
 };
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -12,42 +12,24 @@ const plugins = require("./plugins");
 const isDev = (process.env.NODE_ENV = "development");
 
 let config = {
-  entry: {
-    main: path.resolve(__dirname, "../src/main.js")
-  },
+    entry: {
+        main: path.resolve(__dirname, "../src/main.js")
+    },
 
-  devtool: isDev ? "inline-source-map" : false,
-  mode: process.env.NODE_ENV,
-  module: {
-    rules: loaders
-  },
-  plugins,
-  output: {
-    path: path.resolve(__dirname, "../assets/js"),
-    filename: "[name].js"
-  },
-  optimization: {
-    minimize: true
-  }
+    devtool: isDev ? "inline-source-map" : false,
+    mode: process.env.NODE_ENV,
+    module: {
+        rules: loaders
+    },
+    plugins,
+    output: {
+        path: path.resolve(__dirname, "../assets/js"),
+        filename: "[name].js"
+    },
+    optimization: {
+        minimize: true
+    },
 };
 
-// Add dev server only for development mode and not for production.
-if (isDev) {
-  config.devServer = {
-    devMiddleware: {
-      writeToDisk: true
-    },
-    allowedHosts: "all",
-    host: "localhost",
-    port: 8887,
-    proxy: {
-      "/assets": {
-        pathRewrite: {
-          "^/assets": ""
-        }
-      }
-    }
-  };
-}
 
 module.exports = config;
