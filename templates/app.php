6f63d4d2fdf37d0676b68a485cd84d7c307d39ec
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-10-31 22:10:58 +0600

    update: Notice added if no javascript enabled.

--- templates/app.php
+++ templates/app.php
@@ -1,3 +1,9 @@
 <div id="wp-emailer">
+	<noscript>
+		<strong>
+			<?php esc_html_e( "We're sorry but WpEmailer doesn't work properly without JavaScript. Please enable it to continue.", 'wp-emailer' ); ?>
+		</strong>
+	</noscript>
+
 	<h2><?php esc_html_e( 'Loading', 'wp-emailer' ); ?>...</h2>
 </div>
